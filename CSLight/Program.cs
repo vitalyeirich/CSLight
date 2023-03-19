c2a1ee5a2bdd3a7c59e0301cccc264520ae3ad5a
vitalyeirich <vitalyeirich@gmail.com>
2023-03-19 19:34:02 +0200

    4.1Добавьте файлы проекта.

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -10,8 +10,22 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ЯЮниор!");
-            Console.ReadKey();
+            Console.WriteLine("### Анкета ###\n");
+
+            Console.Write("Как вас зовут?\n");
+            string name = Console.ReadLine();
+
+            Console.Write("\nСколько Вам лет?\n");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nКто вы по профессии?\n");
+            string profession = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("### Информация о Вас ###");
+            Console.WriteLine("Вас зовут " + name + ", вам: " + age + " года/лет, вы " + profession + ".");
+
+            Console.ReadLine();
         }
     }
 }
