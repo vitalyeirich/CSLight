066ef3913cb92a41728b0337cca04b6dcc4a4c13
Vitaly Eirich <vitalyeirich@gmail.com>
2023-03-19 20:09:14 +0200

    Update Program.cs

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +10,18 @@ namespace CSLight
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ЯЮниор!");
-            Console.ReadKey();
+            string spaceshipName = "U.S.S. Discovery One";
+            char spaceshipClassLetter = 'A';
+            bool isSpaceshipActive;
+            int spaceshipMaxHealth = 2000;
+            int spaceshipCurrentHealth = 2000;
+            int spaceshipMaxCrew = 10;
+            int spaceshipCurrentCrew = 8;
+            float spaceshipMaxSpeed = 200;
+            float spaceshipCurrentSpeed = 120.5f;
+            float spaceshipMaxFuel = 100;
+            float spaceshipCurrentFuel = 85.5f;
+            
         }
     }
 }
